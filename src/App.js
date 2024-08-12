0ecbd6f835e7da5627adc11991a626c3bdbc8932
Naveen20R <naveenr6385@gmail.com>
2024-08-13 00:21:08 +0530

    structure created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <div>Frontend</div>
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}>
+            {/* <Route index element={<AgencyModern />} />
+          <Route path='service' element={<ServiceV4 />} />
+          <Route path='mpeoples' element={<Mpeoples />} />
+          <Route path='courses' element={<Courses />} />
+          <Route path='aboutUs' element={<AboutUs />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='cryptoCurrency' element={<Crypto />} />
+          <Route path='studentdetail' element={<StudentDetail />} />
+          <Route path='login' element={<LogIn />} />
+          <Route path='signup' element={<SignUp />} />
+          <Route path='404' element={<NotFound />} />
+          <Route path='*' element={<NotFound />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
